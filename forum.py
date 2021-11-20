74856478c0ede42058c80646eb0220bfba89207c
ehaavisto <eerohaavisto@gmail.com>
2021-11-20 03:16:33 +0200

    continuing work on forums

--- forum.py
+++ forum.py
@@ -27,7 +27,6 @@ def showChain(chain_id):
 		comment_id = row.comment_id
 		query_comments = ("SELECT user_id, comment, timestamp FROM comments WHERE comment_id= "+str(comment_id))
 		get_comments=session.execute(query_comments)
-	#print (get_comments)
 		for comment in get_comments:
 			# Get name of the user_id
 			query_customer_info = ("SELECT first_name, last_name FROM customer_info WHERE customer_id = "+str(comment.user_id))
@@ -37,3 +36,32 @@ def showChain(chain_id):
 			comments.append([name, comment.comment, comment.timestamp])
 	for i in comments:
 		print(i[0] + ": " + i[1] + " on " + i[2].strftime('%Y-%m-%d %H:%M:%S'))
+
+def showBoard(board_id):
+	query_boards = ("SELECT chain_id FROM com_boards WHERE board_id = "+board_id)
+	chains = session.execute(query_boards)
+	for chain in chains:
+		if chain.chain_id:
+			query_chains = ("SELECT chain_name FROM com_chain12 WHERE chain_id = "+str(chain.chain_id))
+			name_set = session.execute(query_chains)
+			for name in name_set:
+				print(name.chain_name)
+
+def createChain(chain_name, board_id):
+	now_iso8601 = datetime.datetime.now()
+	now = str(int(now_iso8601.timestamp()*1000))
+	chain_id = str(uuid.uuid4())
+	query_chains = ("INSERT INTO com_chain12 (chain_id, timestamp, chain_name, board_id) VALUES ("+chain_id+", \'"+now+"\', \'"+chain_name+"\', "+board_id+")")
+	query_boards = ("INSERT INTO com_boards (board_id, timestamp, chain_id) VALUES ("+board_id+", \'"+now+"\', "+chain_id+")")
+	session.execute(query_chains)
+	session.execute(query_boards)
+
+def createBoard(board_name, parent_board_id):
+	now_iso8601 = datetime.datetime.now()
+	now = str(int(now_iso8601.timestamp()*1000))
+	board_id = str(uuid.uuid4())
+	if parent_board_id:
+		query_boards = ("INSERT INTO com_boards (board_id, timestamp, board_name, parent_board_id) VALUES ("+board_id+", \'"+now+"\', \'"+board_name+"\', "+parent_board_id+")")
+	else:
+		query_boards = ("INSERT INTO com_boards (board_id, timestamp, board_name) VALUES ("+board_id+", \'"+now+"\', \'"+board_name+"\')")
+	print(query_boards)
